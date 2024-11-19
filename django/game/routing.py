1b5cf6665cbab5b182f3bd4b6b73479618d1c95f
root <root@ordi-yacine.>
2024-11-19 18:20:56 +0100

    Try to restart with SPA and API endpoints

--- django/game/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-	re_path(r'ws/pong/(?P<party_id>\d+)/(?P<match_id>\d+)/$', consumers.PongConsumer.as_asgi()),
-    re_path(r'ws/pong/(?P<party_id>\w+)/$', consumers.PongConsumer.as_asgi()),
-]
