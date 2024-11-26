fc85be657ef42390b660d14e674505f8104bbe63
Sébastien Froidevaux <seb@MacBook-Air-de-Sebastien.local>
2024-11-26 16:29:58 +0100

    seb

--- django/users/helpers.py
+++ django/users/helpers.py
@@ -4,17 +4,17 @@ import os
 load_dotenv()
 
 champions = [
-    "Garen: The Might of Demacia, a noble warrior with a powerful spinning strike.",
-    "Ahri: The Nine-Tailed Fox, a mage with charm and spirit fire abilities.",
-    "Darius: The Hand of Noxus, a brutal warrior with a deadly axe.",
-    "Lux: The Lady of Luminosity, a light mage with powerful laser attacks.",
-    "Zed: The Master of Shadows, a ninja assassin with shadow clones.",
-    "Ashe: The Frost Archer, an archer with ice arrows and a hawk scout.",
-    "Katarina: The Sinister Blade, a deadly assassin with spinning daggers.",
-    "Teemo: The Swift Scout, a yordle with poisonous darts and stealth.",
-    "Jinx: The Loose Cannon, a chaotic marksman with explosive weapons.",
-    "Yasuo: The Unforgiven, a swordsman with wind-based abilities and a powerful ultimate.",
-    "Veigar: The Tiny Master of Evil, a powerful mage with dark magic and a devastating ultimate."
+	"Musclor : Le Défenseur d’Eternia, un guerrier puissant qui manie l’Épée de Pouvoir et protège les secrets du Château des Ombres.",
+	"Starlion : Le Seigneur des Cosmocats, un héros noble armé de l’Épée d’Omens et guidé par un profond sens de la justice.",
+	"Cobra : L’Aventurier de l’Espace, un aventurier rusé avec un Psychogun et un talent pour déjouer ses ennemis.",
+	"She-Ra : La Princesse du Pouvoir, une championne intrépide avec l’Épée de Protection, en mission pour libérer Etheria.",
+	"Duke : Le Leader de G.I. Joe, un commandant courageux qui lutte pour la liberté contre les forces de Cobra.",
+	"Optimus Prime : Le Chef des Autobots, un robot sage et puissant qui se transforme pour défendre la Terre contre les Decepticons.",
+	"Capitaine Flam : Le Protecteur de l’Univers, un stratège brillant qui défend la galaxie contre les menaces interstellaires.",
+	"Albator : Le Corsaire de l’Espace, un capitaine audacieux de l’Arcadia qui combat la tyrannie pour protéger la liberté de la Terre.",
+	"Jem : La Star Glamour, une chanteuse aux pouvoirs holographiques qui inspire et protège avec son groupe, les Holograms.",
+	"Seiya : Le Chevalier de Pégase, un guerrier courageux qui canalise la puissance du cosmos pour combattre l’injustice.",
+	"Goldorak : Le Protecteur Cosmique, un robot géant piloté par Actarus pour défendre la Terre contre les forces de Véga."
 ]
 
 def pick_random_description():
